c02dbb3c563a9d46e7c1702d4e24b4657c8987fc
Adrian Olariu <adrian.olariu93@gmail.com>
2022-10-26 14:44:22 +0300

    logOutController: delete some console.logs, added a warning comment

--- Server/controllers/logOutController.js
+++ Server/controllers/logOutController.js
@@ -8,15 +8,15 @@ const DB = {
 };
 
 async function logOut(req, res){
+
+    //-------------------------WARNING: TO ADD VERIFICATIONS
     const cookie = req.cookies;
     console.log('cookie here:', cookie);
     const foundUser = DB.users.find(user => user.refreshToken === cookie.jwt);
     console.log(foundUser);
     const otherUsers = DB.users.filter(user => user.refreshToken !== foundUser.refreshToken);
     foundUser.refreshToken = '';
-    // console.log('other users',otherUsers);
     DB.setUsers([...otherUsers, foundUser]);
-    // console.log('other users',DB.users);
     //STERGEM COOKIE-ul
     res.clearCookie('jwt', {httpOnly: true, sameSite:'None', secure:true});
     res.sendStatus(204);
